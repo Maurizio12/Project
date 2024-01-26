bf9c94ef0844c62a135fb36073ffa99ecf7d5ed6
Maurizio12 <mauriziogizzi12@gmail.com>
2024-01-26 13:43:44 +0000

    add javascript

---
+++ java/Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello word';
+console.log(greeting1);
+
+var greeting2 = 'welcome to node js';
+console.log(greeting2);
+
+var sentence1 = 'learn how to debug code';
+console.log(sentenceone);
